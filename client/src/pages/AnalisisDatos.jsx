fd38b26c827a07015b2080815f9159d6feac6fc1
Marco Pavez <marcopz03@outlook>
2023-02-02 14:34:59 -0300

    Avance Servicio AnalisisDatos.jsx

--- client/src/pages/AnalisisDatos.jsx
+++ client/src/pages/AnalisisDatos.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import arrange_qqplot from "../img/arrange_qqplot.png"
 
 function AnalisisDatos() {
   return (
-    <div>AnalisisDatos</div>
+    <div className="container">
+      <div className="analisisDatos">
+        <h1>Análisis de datos</h1>
+        <h3>Refiere a la aplicación de técnicas estadísticas y computaciales a un set de datos prestablecido</h3>
+      </div>
+      <div className="ejemploAnalisisDatos">
+        <img src={arrange_qqplot} alt="Error al cargar imagen"/>
+      </div>
+    </div>
   )
 }
 
