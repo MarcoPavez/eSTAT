bbb27809c56cef01c1aab60ef30a8bf4b2604a5c
Marco Pavez <marcopz03@outlook>
2023-02-17 00:05:13 -0300

    avance LimpiezaDatos.jsx y RecoleccionDatos.jsx

--- client/src/pages/RecoleccionDatos.jsx
+++ client/src/pages/RecoleccionDatos.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function RecoleccionDatos() {
   return (
-    <div>RecoleccionDatos</div>
+    <div className="container">
+      <div className="recoleccionDatos">
+        <h1>Recolección de datos</h1>
+        <h3>Refiere a la aplicación de técnicas computacionales para extraer datos. Las fuentes de información pueden ser APIs, textos, páginas web, entre otras</h3>
+      </div>
+      <div className="inicioEjemplo">
+        <h3>Te presentamos dos ejemplos para clarificar el proceso:</h3>
+        <p>1.- Queremos extraer </p>
+      </div>
+    </div>
   )
 }
 
