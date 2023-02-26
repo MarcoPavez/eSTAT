bbbdd4e5b565c62f677cbc05bbe2a945a2f6fa47
Marco Pavez <marcopz03@outlook>
2023-02-26 20:21:35 -0300

    Cambios en descripciones de las páginas de cada servicio; cambios visual en footer

--- client/src/pages/RecoleccionDatos.jsx
+++ client/src/pages/RecoleccionDatos.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function RecoleccionDatos() {
   return (
     <div className="container">
       <div className="recoleccionDatos">
         <h1>Recolección de datos</h1>
-        <h3>Refiere a la aplicación de técnicas computacionales para extraer datos. Las fuentes de información pueden ser APIs, textos, páginas web, entre otras</h3>
+        <h3>
+          Refiere a la aplicación de técnicas computacionales para extraer datos
+          de distintas fuentes de información, como textos, páginas web e
+          inclusive el consumo de APIs públicas, entre otras.
+        </h3>
       </div>
       <div className="inicioEjemplo">
-        <h3>Te presentamos dos ejemplos para clarificar el proceso:</h3>
-        <p>1.- Queremos extraer </p>
+        <h3>Te presentamos un ejemplo para clarificar el proceso:</h3>
+        <p>
+          1.- Supón que quieres realizar un análisis de texto desde el siguiente{" "}
+          <a href="https://s3.amazonaws.com/gobcl-prod/public_files/Campa%C3%B1as/Cuenta-P%C3%BAblica-2020/MENSAJE-PRESIDENCIAL-2020.pdf">
+            documento
+          </a>
+          , que es el mensaje presidencial realizado durante la Cuenta Pública
+          del año 2020. Te gustaría saber cuales son las palabras más frecuentes
+          del discurso y obtener conclusiones de ello. Para realizarlo, ejecutas
+          un análisis de <i>Term Frequency – Inverse Document Frequency</i> (
+          <a href="https://www.geeksforgeeks.org/understanding-tf-idf-term-frequency-inverse-document-frequency/">
+            TF-IDF
+          </a>
+          ), que significa frecuencia de término – frecuencia inversa de
+          documento
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default RecoleccionDatos
+export default RecoleccionDatos;
